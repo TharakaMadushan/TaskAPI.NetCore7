53f61bbf78510092d8239b148125792ce6bc86d6
TharakaMadushan <madushan@perfectbss.com>
2022-12-11 12:03:45 +0530

    20221211-2

---
+++ TaskAPI/Controllers/TodosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTodos() { return Ok(); }
+    }
+}
---
+++ TaskAPI/Models/TodoStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskAPI.Models
+{
+    public enum TodoStatus
+    {
+        New,
+        InProgress,
+        Completed
+    }
+}
