3e731c51a2e9914bcaf498124fe0cd39213211a2
TharakaMadushan <madushan@perfectbss.com>
2022-12-31 14:46:41 +0530

    20221231-4

--- TaskAPI.Services/Models/CreateToDoDTO.cs
+++ TaskAPI.Services/Models/CreateToDoDTO.cs
@@ -10,8 +10,8 @@ namespace TaskAPI.Services.Models
 {
     public class CreateToDoDTO
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public DateTime Created { get; set; }
         public DateTime Due { get; set; }
         public TodoStatus Status { get; set; }
